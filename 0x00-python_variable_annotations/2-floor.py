b7bd15d0a670f8cc7e87f8a1a984cec92a9fefc0
sosthene <sosthenetimi94@gmail.com>
2023-12-07 16:40:28 +0300

    Flooring

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function floor which takes float 'n'
+and returns floor of the arg
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float parameter and returns an int
+    """
+    return math.floor(n)
