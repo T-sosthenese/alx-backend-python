3f2adb0760d35816969f8add6c8a662e8aa5f540
sosthene <sosthenetimi94@gmail.com>
2023-12-07 16:15:40 +0300

    Concatenating two strings

---
+++ 1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Documentation for for concat function that
+takes 2 strings args and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes in two strings and concatenates them.
+    """
+    return str1 + str2
