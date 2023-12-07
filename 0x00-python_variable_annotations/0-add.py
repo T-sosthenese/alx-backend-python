99951cc61225739cffd9c050799639d4679e039c
sosthene <sosthenetimi94@gmail.com>
2023-12-07 15:39:33 +0300

    Type-annotated sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A type-annotated function add(0 that takes float a and
+float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two foat args and returns their sum in float format
+    """
+    return a + b
