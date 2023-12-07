7f476405e9dd8c94c2e9e7af18574390135fd13e
sosthene <sosthenetimi94@gmail.com>
2023-12-07 18:44:26 +0300

    Duck-typed functions

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module implementation
+"""
+
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Duck-typed annotations
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
