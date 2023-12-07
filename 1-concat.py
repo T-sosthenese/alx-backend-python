94bf0fb84dea3bdf19f94db318f1de5d944e9872
sosthene <sosthenetimi94@gmail.com>
2023-12-07 16:21:30 +0300

    Concatenating strings

--- 1-concat.py
+++ 1-concat.py
@@ -7,6 +7,6 @@ takes 2 strings args and returns a concatenated string
 
 def concat(str1: str, str2: str) -> str:
     """
-    Takes in two strings and concatenates them.
+    Takes in two strings and concatenates them
     """
     return str1 + str2
