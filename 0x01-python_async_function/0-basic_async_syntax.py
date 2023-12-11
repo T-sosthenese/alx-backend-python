6f0acb09d7d7a31c0c5d8a599f3c634d2a49b09c
sosthene <sosthenetimi94@gmail.com>
2023-12-11 14:44:13 +0300

    asynchronous random numbers

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module implementation for async function
+"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    An asynchronous function that returns a random float
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
