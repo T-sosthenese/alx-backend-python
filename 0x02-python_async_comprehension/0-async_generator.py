f633168acb43dcf52cdf8c32d7c78fa0e0c9e73e
sosthene <sosthenetimi94@gmail.com>
2023-12-13 00:09:34 +0300

    asynchronous generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ A module that yields asynchronous values
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() ->Generator[float, None, None]:
     """
     An asynchronous function that generates asynchronous values
     without taking args
